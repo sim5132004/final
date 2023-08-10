ecb911789707a8858c9646b2f52ff4722ec66b27
chung219 <a01039229675@gmail.com>
2023-08-10 16:29:35 +0900

    초기사항

---
+++ 스프링부트/src/main/java/party/people/config/Config.java
@@ -0,0 +1,9 @@
+package party.people.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class Config {
+}
