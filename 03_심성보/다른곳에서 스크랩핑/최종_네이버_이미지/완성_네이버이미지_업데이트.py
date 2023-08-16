cc21f8e7ff5b83fa6fef423f3a64821e12910176
sim5132004 <129469224+sim5132004@users.noreply.github.com>
2023-08-16 10:49:44 +0900

    aaaa

--- 03_심성보/다른곳에서 스크랩핑/최종_네이버_이미지/완성_네이버이미지_업데이트.py
+++ 03_심성보/다른곳에서 스크랩핑/최종_네이버_이미지/완성_네이버이미지_업데이트.py
@@ -16,19 +16,18 @@ with open('search_list.txt', 'r') as f:
     search_list = f.read()
     f.close()
 search_list_s=search_list.split('\n')
-
+#아래는 테스트 데이터
+# search_list_s=['돈비어천가','동암아구해물탕','등나무가든','마포소금구이']
 def update_image_from_oracle(target_url,list_name,count):
     con = cx_Oracle.connect('party/party@192.168.30.240:1521/xe')
     cur = con.cursor()
+    # 데이터2 -> 테이블명 변경
     if count ==1:
-        sql = "update 데이터2 set 이미지주소1  = :1  WHERE 제목 =:2"
+        sql = "update placeinfo set 이미지주소1  = :1  WHERE 제목 =:2"
         cur.execute(sql, ( target_url, list_name))
     else:
-        sql = "update 데이터2 set 이미지주소2  = :1  WHERE 제목 =:2"
+        sql = "update placeinfo set 이미지주소2  = :1  WHERE 제목 =:2"
         cur.execute(sql, (target_url, list_name))
-    # sql = "update 데이터2 set :1  = :2  WHERE 제목 =:3"
-    # cur.execute(sql,(txt,target_url,list_name))
-    #
 
     con.commit()
     cur.close()
@@ -37,7 +36,7 @@ def update_image_from_oracle(target_url,list_name,count):
 
 
 count=0
-search_list_s=['돈비어천가','동암아구해물탕','등나무가든','마포소금구이']
+
 for go in search_list_s:
 
     try:
@@ -64,20 +63,6 @@ for go in search_list_s:
                 print(f"{img_list['link']} {i}")
                 print(search_list_s[count])
                 update_image_from_oracle(img_list['link'],search_list_s[count],i)
-
-                # 저장 파일명 및 경로
-                # FileName = f"{savePath}{go}_{i}.jpg"
-                # os.path.join(savePath, savePath + go+"_"str(i) + '.jpg')
-
-                # 파일명 출력
-                # print('full name : {}'.format(FileName))
-
-                # 이미지 다운로드 URL 요청
-                # 이 부분에 데이터베이스에 url을 저장하는 코드 생성
-                # urllib.request.urlretrieve(img_list['link'], FileName)
-
-            # 다운로드 완료 시 출력
-            # print("--------download succeeded--------")
 
         else:
             print("Error Code:" + rescode)
