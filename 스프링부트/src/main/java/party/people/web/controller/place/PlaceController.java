cba9ea57d8c5dabf5841710f1919ab46eb238280
chung219 <a01039229675@gmail.com>
2023-08-22 15:12:01 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- 스프링부트/src/main/java/party/people/web/controller/place/PlaceController.java
+++ 스프링부트/src/main/java/party/people/web/controller/place/PlaceController.java
@@ -10,13 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import party.people.domain.Page;
 import party.people.domain.Place;
-//import party.people.domain.TestOne;
 import party.people.repository.place.PlaceInterface;
 
-import static party.people.service.keyword.keywordToMapLogic.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,43 +30,6 @@ public class PlaceController {
 
         return "place/placeTest";
     }
-
-//    @PostMapping("go_invite")
-//    public String goInvite(@RequestParam("go_invite") TestOne testOne, Model model){
-//
-//
-//        return "testpage/test_shim_in";
-//    }
-//
-
-//    @GetMapping("test_main")
-//    public String testMain(Model model){
-//        TestOne one = new TestOne();
-//        System.out.println("셋터 시작");
-//        one.setOne(1);
-//        one.setTwo(2);
-//        one.setThree(3);
-//
-//        model.addAttribute("testOne", one);
-//        System.out.println("리턴 시작");
-//        return "testpage/test_shim_pl";
-//    }
-
-//
-////    @PostMapping("test_main")
-//    public String postMain(@ModelAttribute("test_ii") TestOne testOne, Model model){
-//        TestOne testOne1= new TestOne();
-//        model.addAttribute("c1", testOne1);
-//        return "";
-//    }
-//
-//    @PostMapping("test_i")
-//    public String testIn(@ModelAttribute("test_ii") TestOne testOne, Model model){
-////        Integer s=testOne.getOne();
-////        System.out.println(s);
-//        return "testpage/test_shim_in";
-//    }
-//
 
 
 
