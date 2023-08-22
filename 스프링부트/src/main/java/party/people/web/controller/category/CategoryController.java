5933d225742a220bf3d0258488cbf7d7b537a9c9
chung219 <a01039229675@gmail.com>
2023-08-22 16:11:38 +0900

    지금 페이지들 쪼갬
    쪼갬

--- 스프링부트/src/main/java/party/people/web/controller/category/CategoryController.java
+++ 스프링부트/src/main/java/party/people/web/controller/category/CategoryController.java
@@ -1,14 +1,23 @@
 package party.people.web.controller.category;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CategoryController {
 
     @GetMapping("place")
-    public String goPlace(){
-        return "place/place_CK";
+    public String goPlace(Model model){
+        model.addAttribute("category","place");
+        return "place/place_ck";
+    }
+
+
+    @GetMapping("invite")
+    public String goInvite(Model model){
+        model.addAttribute("category","invite");
+        return "invite/invite_ck";
     }
 
 
