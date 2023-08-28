39b71e6d03087e6cd09506a0b0e000bb21355a60
chung219 <a01039229675@gmail.com>
2023-08-28 17:16:40 +0900

    myCard

--- 스프링부트/src/main/java/party/people/web/controller/category/CategoryController.java
+++ 스프링부트/src/main/java/party/people/web/controller/category/CategoryController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import party.people.domain.Client;
+import party.people.domain.InviteCard;
 import party.people.domain.Place;
 import party.people.domain.SearchResult;
+import party.people.repository.InviteCard.InviteCardInterface;
 import party.people.repository.place.PlaceInterface;
 import party.people.repository.search.SearchInputInterface;
 
@@ -28,6 +30,7 @@ public class CategoryController {
 
     private final PlaceInterface placeInterface;
     private final SearchInputInterface searchInputInterface;
+    private final InviteCardInterface inviteCardInterface;
 
     @GetMapping("place")
     public String goPlace(HttpServletRequest request, Model model){
@@ -122,73 +125,97 @@ public class CategoryController {
         /* 함수 정보는 함수 이름 컨트롤 클릭 */
         loginCheck(request, model);
 
-        request.getSession();
+        HttpSession session = request.getSession(false);
+        Client info = (Client) session.getAttribute("로그인");
 
-
-
-        Random random = new Random();
-
-        int randomNumber1 = random.nextInt(1,10);
-        int randomNumber2 = random.nextInt(1,800);
-        int randomNumber3 = random.nextInt(1, 800);
-
-
-        List<Place> placeList2 = new ArrayList<>();
-        Place place12 = placeInterface.idSearch(randomNumber1);
-        /* 이미지 다운로드용 에러방지 이미지 주소를 변환 인코딩 */
-        placeImageToBinary(place12);
-        Place place22 = placeInterface.idSearch(randomNumber2);
-        placeImageToBinary(place22);
-        Place place32 = placeInterface.idSearch(randomNumber3);
-        placeImageToBinary(place32);
-        placeList2.add(place12);
-        placeList2.add(place22);
-        placeList2.add(place32);
-
-        place12.keyWord5();
-
-
-        /* 플레이스에서 보내져온 리스트 확인용 세션 */
-        HttpSession test = request.getSession(false);
-        /* 플레이스 리스트 객체를 담을 전역(컨트롤러 한정) 리스트변수 생성 */
-        List<Place>place2 = new ArrayList<>();
-
-        /* 세션 값이 있을 경우 이하 구문 실행 */
-        if (test!=null) {
-            /* 세션 중 플레이스에서 보내온 세션이 있는지 확인하고 있으면 플레이스 리스트 객체에 저장 */
-
-            /* 키값을 "검색결과" 로 받아오면 인바이트에서 List<Place>가 아니라
-               List<List<Place>를 받아오는 경우에 에러가 생겨서
-                세션 키값을 검색결과와 선택결과로 분리 */
-            place2 = (List<Place>) test.getAttribute("선택결과");
-
-            /* 그냥 Invite페이지에 왔을때를 위해 선택결과 세션 초기화 */
-            test.setAttribute("선택결과",null);
-            log.info("세션에서 받아온 정보 확인 "+place2);
-            /* "검색결과"키로 세션 정보를 받은 경우 */
-            if (place2 != null) {
-                /* 이미지 주소 다운 에러 방지용 base64타입 인코딩 객체를 담을 리스트 생성 */
-                List<Place>newImageAddPlace = new ArrayList<>();
-
-                /* 각 객체의 이미지 주소를 다운로드 시 에러방지용으로 base64타입으로 인코딩 */
-                for (Place obj : place2){
-                    placeImageToBinary(obj);
-                    newImageAddPlace.add(obj);
-                }
-                Place pl = newImageAddPlace.get(0);
-
-                /* 해당 리스트를 타임리프단으로 전달 */
-                model.addAttribute("category2", newImageAddPlace);
-                /* 인바이트 제목내용 전달 */
-                model.addAttribute("keylist",pl.keyWordTitle());
-                /* 세션 정보가 없을 경우 랜덤생성된 리스트 타임리프로 전달 */
-            } else model.addAttribute("category2",placeList2);
+        log.info("로그인 세션까지 확인 ");
+        List<InviteCard> list = inviteCardInterface.loadById(info.getClientId());
+        log.info("리스트 확인 "+list);
+        List<Place> listPlace = new ArrayList<>();
+        if (!list.isEmpty()){
+            for (InviteCard one : list){
+                Integer placeId1 = one.getPLACE_ID_1();
+                Integer placeId2 = one.getPLACE_ID_2();
+                Integer placeId3 = one.getPLACE_ID_3();
+                Place place1 = placeInterface.idSearch(placeId1);
+                placeImageToBinary(place1);
+                Place place2 = placeInterface.idSearch(placeId2);
+                placeImageToBinary(place2);
+                Place place3 = placeInterface.idSearch(placeId3);
+                placeImageToBinary(place3);
+                listPlace.add(place1);
+                listPlace.add(place2);
+                listPlace.add(place3);
+                break;
+            }
         }
-        /* 세션 정보가 없을 경우 랜덤생성된 리스트 타임리프로 전달 */
-        else{
-            model.addAttribute("category2", placeList2);
-            log.info("NULL !!!!");
-        }
+        model.addAttribute("category2", listPlace);
+
+
+
+//        Random random = new Random();
+//
+//        int randomNumber1 = random.nextInt(1,10);
+//        int randomNumber2 = random.nextInt(1,800);
+//        int randomNumber3 = random.nextInt(1, 800);
+//
+//
+//        List<Place> placeList2 = new ArrayList<>();
+//        Place place12 = placeInterface.idSearch(randomNumber1);
+//        /* 이미지 다운로드용 에러방지 이미지 주소를 변환 인코딩 */
+//        placeImageToBinary(place12);
+//        Place place22 = placeInterface.idSearch(randomNumber2);
+//        placeImageToBinary(place22);
+//        Place place32 = placeInterface.idSearch(randomNumber3);
+//        placeImageToBinary(place32);
+//        placeList2.add(place12);
+//        placeList2.add(place22);
+//        placeList2.add(place32);
+//
+//        place12.keyWord5();
+//
+//
+//        /* 플레이스에서 보내져온 리스트 확인용 세션 */
+//        HttpSession test = request.getSession(false);
+//        /* 플레이스 리스트 객체를 담을 전역(컨트롤러 한정) 리스트변수 생성 */
+//        List<Place>place2 = new ArrayList<>();
+//
+//        /* 세션 값이 있을 경우 이하 구문 실행 */
+//        if (test!=null) {
+//            /* 세션 중 플레이스에서 보내온 세션이 있는지 확인하고 있으면 플레이스 리스트 객체에 저장 */
+//
+//            /* 키값을 "검색결과" 로 받아오면 인바이트에서 List<Place>가 아니라
+//               List<List<Place>를 받아오는 경우에 에러가 생겨서
+//                세션 키값을 검색결과와 선택결과로 분리 */
+//            place2 = (List<Place>) test.getAttribute("선택결과");
+//
+//            /* 그냥 Invite페이지에 왔을때를 위해 선택결과 세션 초기화 */
+//            test.setAttribute("선택결과",null);
+//            log.info("세션에서 받아온 정보 확인 "+place2);
+//            /* "검색결과"키로 세션 정보를 받은 경우 */
+//            if (place2 != null) {
+//                /* 이미지 주소 다운 에러 방지용 base64타입 인코딩 객체를 담을 리스트 생성 */
+//                List<Place>newImageAddPlace = new ArrayList<>();
+//
+//                /* 각 객체의 이미지 주소를 다운로드 시 에러방지용으로 base64타입으로 인코딩 */
+//                for (Place obj : place2){
+//                    placeImageToBinary(obj);
+//                    newImageAddPlace.add(obj);
+//                }
+//                Place pl = newImageAddPlace.get(0);
+//
+//                /* 해당 리스트를 타임리프단으로 전달 */
+//                model.addAttribute("category2", newImageAddPlace);
+//                /* 인바이트 제목내용 전달 */
+//                model.addAttribute("keylist",pl.keyWordTitle());
+//                /* 세션 정보가 없을 경우 랜덤생성된 리스트 타임리프로 전달 */
+//            } else model.addAttribute("category2",placeList2);
+//        }
+//        /* 세션 정보가 없을 경우 랜덤생성된 리스트 타임리프로 전달 */
+//        else{
+//            model.addAttribute("category2", placeList2);
+//            log.info("NULL !!!!");
+//        }
 
         return "client/myCard";
     }
